35689403b00999d4e73870e6e15a73307d1d6e17
Kenneth Nielsen <k.nielsen81@gmail.com>
2022-01-21 09:15:33 +0100

    Improve cdl

--- cdl.fish
+++ cdl.fish
@@ -5,13 +5,25 @@ function cdl -d "Find a folder case insensitive that starts with"
 	return 0
     end
 
+    # First test for arg at begining of string
     set search_term (string lower $argv[1])
     for file in *
 	set lfile (string lower $file)
 	set same_length_lfile (string sub -s 1 -l (string length $search_term) $lfile)
 	if [ $search_term = $same_length_lfile ]; and test -d $file
 	    cd $file
-	    break
+	    return 0
 	end
     end
+
+    # Then test for arg anywhere in filename
+    for file in *
+        set lfile (string lower $file)
+	if [ (string match "*$search_term*" $lfile) ]; and test -d $file
+	   cd $file
+	   break
+	end
+    end
+
+	
 end
